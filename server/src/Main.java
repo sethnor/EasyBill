fead2b02f75cbfb6b82c3e03e50ad3063686ff95
jacqueslorentz <jacques.lorentz@epitech.eu>
2016-05-10 12:24:22 +0200

    Add server idea file

--- server/src/Main.java
+++ server/src/Main.java
@@ -2,17 +2,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String host = "127.0.0.1";
+        String host = "10.14.59.120";
         int port = 2345;
 
         Server ts = new Server(host, port);
         ts.start();
 
-/*
-        for(int i = 0; i < 1; i++){
+
+        for(int i = 0; i < 0; i++){
             Thread t = new Thread(new ClientConnexion(host, port));
             t.start();
         }
-*/
+
     }
 }
