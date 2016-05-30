2cfe300d7693405492b90ee1468f6b55174c3598
sethnor <sami.skalli@epitech.eu>
2016-05-31 01:48:42 +0200

    capture picture

--- client/EasyBill/app/src/main/java/eu/epitech/sami/easybill/PhotoActivity.java
+++ client/EasyBill/app/src/main/java/eu/epitech/sami/easybill/PhotoActivity.java
@@ -3,11 +3,19 @@ package eu.epitech.sami.easybill;
 import android.app.Activity;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class PhotoActivity extends Activity {
 
@@ -26,7 +34,7 @@ public class PhotoActivity extends Activity {
             @Override
             public void onClick(View arg0) {
                 Log.d("status", "capture image");
-
+                camera.takePicture(null, null, picture);
             }
 
         });
@@ -45,5 +53,44 @@ public class PhotoActivity extends Activity {
         }
         else
             Log.d("status", "camera null");
+    }
+
+    Camera.PictureCallback picture = new Camera.PictureCallback() {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            File pictureFile = getOutputMediaFile();
+            if (pictureFile == null) {
+                return;
+            }
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+            } catch (FileNotFoundException e) {
+
+            } catch (IOException e) {
+            }
+        }
+    };
+
+    private static File getOutputMediaFile() {
+        File mediaStorageDir = new File(
+                Environment
+                        .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                "MyCameraApp");
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
+                .format(new Date());
+        File mediaFile;
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator
+                + "IMG_" + timeStamp + ".jpg");
+
+        return mediaFile;
     }
 }
