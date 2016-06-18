65e6cc8977da641345c00018d36422a8a37e654f
jacqueslorentz <jacques.lorentz@epitech.eu>
2016-06-18 13:51:44 +0200

    Modif and add doc server

--- server/src/Main.java
+++ server/src/Main.java
@@ -2,17 +2,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String host = "10.14.59.180";
+        String host = "127.0.0.1";//"10.14.59.180";
         int port = 2345;
 
         Server ts = new Server(host, port);
         ts.start();
 
-
-        for(int i = 0; i < 0; i++){
+        /* Testing */
+        /*
+        for(int i = 0; i < 1; i++){
             Thread t = new Thread(new ClientConnexion(host, port));
             t.start();
         }
-
+        */
     }
 }
